e4143986a24c8fc3615f5f7a7b713d5a94c90afb
Alwin Baby <alwin.baby@diagnal.com>
2022-10-05 00:05:46 +0530

    changes

--- src/components/headerBar/LoginButton.jsx
+++ src/components/headerBar/LoginButton.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function LoginButton() {
   return (
     <div className='headerbar2'>
-      <button className='log' type='submit'>LOGIN</button>
-    </div>
-  )
-}
+    <button className='log' type='submit'>LOGIN</button>
+  </div>
+  )}
+
 
 export default LoginButton
