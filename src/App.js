8bf764be614223cba33c11f3ba513ff3c8f5cdeb
Alwin Baby <alwin.baby@diagnal.com>
2022-10-05 00:09:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from "react";
-
-// import { Routes,Route} from 'react-router-dom'
-import LandingPage from "./pages/LandingPage.jsx";
-import SignUpPage from "./pages/SignUpPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-
+import signUpPage from "./pages/SignUpPage";
 
 function App() {
     return (
     <>
-
-            {/* <Routes> */}
-
-            {/* <Route exact path = "/" component = {LandingPage} /> */}
-            {/* <Route path = '/loginpage'><LoginPage/></Route>
-            <Route path = '/signuppage'><SignUpPage/></Route> */}
-
-            {/* </Routes> */}
-    
-        <LandingPage/>
-        {/* <LoginPage/> */}
-        {/* <SignUpPage/> */}
+        <signUpPage/>
     </>
     )
 }
