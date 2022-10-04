bcd08d488c6f28879f26ae5ad9abaf144784722d
Alwin Baby <alwin.baby@diagnal.com>
2022-10-04 23:51:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
             {/* </Routes> */}
     
-        {/* <LandingPage/> */}
+        <LandingPage/>
         {/* <LoginPage/> */}
         {/* <SignUpPage/> */}
     </>
