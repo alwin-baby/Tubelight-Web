838c1e7de842bca6d8a249592063cd4b607e3b97
akshayjayan72 <111509454+akshayjayan72@users.noreply.github.com>
2022-10-06 11:16:48 +0530

    signuppage dom
    dom updated

--- src/components/body/LoginPageBody.jsx
+++ src/components/body/LoginPageBody.jsx
@@ -60,7 +60,7 @@ function LoginPageBody() {
                                 </td>
                                 <td className={classes.tdContainer}>
                                     <p className={classes.forgotPassword}>
-                                        <a>Forgot password ?</a>
+                                        <>Forgot password ?</>
                                     </p>
                                 </td>
                             </tr>
--- src/components/body/SignupPageBody.jsx
+++ src/components/body/SignupPageBody.jsx
@@ -13,49 +13,72 @@ function SignupPageBody() {
   return (
     <form className='fbox'>
       
-      <table className='tbox'>
       
+      <div className='welcomeContainer'>
       <p className='welcome'>Welcome !</p>
+      </div>
+      <table className='tbox'>
+        <tbody>
         <tr>
+          <td>
         <label className='fname'>Name</label>
         <br></br><br></br>
         <input type="text" placeholder='Enter your full name' className='iname'></input>
+        </td>
         </tr>
 
         <tr>
+          <td>
         <br></br> 
         <label className='fname'>Email</label>
         <br></br> <br></br> 
         <input type="text" placeholder='Enter your email' className='iname'></input>
+        </td>
         </tr>
 
         <tr>
+          <td>
         <br></br> 
         <label className='fname'>UserName</label>
         <br></br><br></br> 
         <input type="text" placeholder='Enter your user name' className='iname'></input>
+        </td>
         </tr>
 
         <tr>
+        <td>
         <br></br> 
         <label className='fname'>Password</label>
         <br></br><br></br> 
         <input type="password" placeholder='Enter your Password' className='iname' ></input>
+        </td>
         </tr>
 
         <tr>
+          <td>
         <br></br> 
         <label className='fname'>Confirm Password</label>
         <br></br><br></br> 
         <input type="password" placeholder='Confrim your Password' className='iname' ></input>
+        </td>
         </tr>
 
-        <br></br>
+       
+        <tr>
+          <td>
+          <br></br>
         <br></br> 
         <button className='signup'>Signup</button>
         <br></br>
+        </td>
+        </tr>
         
+        <tr>
+          <td>
         <p className="Already">Already have an account ? <Link to='/login' className='login' href >Login</Link> </p>
+          </td>
+        </tr>
+        </tbody>
         </table>
     </form>
   )
