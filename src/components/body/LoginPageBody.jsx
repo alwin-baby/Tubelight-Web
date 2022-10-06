b5bea866c140ddc7ce35684ed487cbaf6d789331
Alwin Baby <alwin.baby@diagnal.com>
2022-10-06 10:01:40 +0530

    Login page css

--- src/components/body/LoginPageBody.jsx
+++ src/components/body/LoginPageBody.jsx
@@ -1,49 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// import classes from "./LoginPageBody.module.css";
+import classes from "./LoginPageBody.module.css";
 // eslint-disable-next-line
 // import style from "./style.css";
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function LoginPageBody() {
     return (
-        <div>
-            <div>
-                <p>Welcome !</p>
+        <div className={classes.bodyContainer}>
+            <div className={classes.welcomeContainer}>
+                <p className={classes.welcome}>Welcome !</p>
             </div>
-            <div>
+            <div className={classes.formContainer}>
                 <form>
                     <table>
-                        <tr>
-                            <input
-                                type="text"
-                                placeholder="Enter your user name"
-                            ></input>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td colSpan="3">
+                                    <p className={classes.userName}>
+                                        User name
+                                    </p>
+                                </td>
+                            </tr>
 
-                        <tr>
-                            <input
-                                type="password"
-                                placeholder="Enter your Password"
-                            ></input>
-                        </tr>
-                        <div>
-                            <input type="checkbox" name="checkbox"></input>
-                            <p>Remember Me</p>
-                        </div>
-                        <div>
-                            <p>
-                                <a href>Forgot password ?</a>
-                            </p>
-                        </div>
-                        <button>Login</button>
+                            <tr>
+                                <td colSpan="3">
+                                    <input
+                                        type="text"
+                                        placeholder="Enter your user name"
+                                    ></input>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td colSpan="3">
+                                    <p className={classes.password}>Password</p>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td colSpan="3">
+                                    <input
+                                        type="password"
+                                        placeholder="Enter your Password"
+                                    ></input>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className={classes.checkboxContainer}>
+                                    <input
+                                        className={classes.checkbox}
+                                        type="checkbox"
+                                        name="checkbox"
+                                    ></input>
+                                </td>
+                                <td>
+                                    <p className={classes.rememberMe}>Remember Me</p>
+                                </td>
+                                <td className={classes.tdContainer}>
+                                    <p className={classes.forgotPassword}>
+                                        <a href>Forgot password ?</a>
+                                    </p>
+                                </td>
+                            </tr>
+                        </tbody>
                     </table>
                 </form>
             </div>
             <div>
-                <p>
-                    Don't have an account ? <Link to="/signup">Register</Link>
+                <button>Login</button>
+            </div>
+            <div className={classes.registerContainer}>
+                <p className={classes.footerText}>
+                    Don't have an account ? <Link className={classes.register} to="/signup">Register</Link>
                 </p>
             </div>
         </div>
